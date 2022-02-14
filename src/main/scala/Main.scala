b54388965d9ea79e9f5021a033f0fad9a0c5fc94
cameronlimRev <cameron.lim@revature.net>
2022-02-14 13:06:40 -0800

    Test working with DF and JSON

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,16 @@
+import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]):Unit = {
-    println("Hello World")
+
+    val spark = SparkSession.builder()
+      .appName("I wonder if this matters")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.read.json("input/yelp_academic_dataset_business.json")
+    df.select("business_id").where("attributes = 'RestaurtantsTakeout'").show(1000)
   }
 }
