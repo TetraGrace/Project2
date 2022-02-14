aa4a40792dee0c5faa5d5315a2a97340746b3971
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-02-14 15:57:09 -0500

    test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,18 @@
 
+import org.apache.spark.sql._
+import scala.io.StdIn._
+
 object Main {
   def main(args: Array[String]):Unit = {
-    println("Hello World")
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+    val spark = SparkSession
+      .builder()
+      .appName("Project2")
+      .config("spark.master", "local")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val table = spark.read.json()
   }
 }
