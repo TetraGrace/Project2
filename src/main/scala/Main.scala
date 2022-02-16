471b25e156355329b28d9ef5a20b37d6566d52ed
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-02-16 16:47:45 -0500

    finished q4

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 
-import org.apache.spark.sql._
-import scala.io.StdIn._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]):Unit = {
@@ -8,11 +8,30 @@ object Main {
 
     val spark = SparkSession
       .builder()
-      .appName("Project2")
-      .config("spark.master", "local")
+      .appName("project1")
+      .config("spark.master","local")
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val table = spark.read.json()
+    val table = spark.read.json("data/yelp_academic_dataset_business.json")
+
+    val df=table.select(col("attributes.*")) //all attributes
+    val dfRest=df.filter(col("RestaurantsTakeOut") === "True" || col("RestaurantsTakeOut") === "False")
+    //dfRest.show()
+    //println(dfRest.count)
+
+
+    // Getting table of restaurants only
+    val templist = Seq("True", "False")
+    val tableRest = table.filter(table("attributes")("RestaurantsTableService").isin(templist:_*));
+    tableRest.show();
+    //println(tableRest.count())
+
+    //  4. Rating to review count comparison. To gauge validity of reviews to the avg review score
+    val q4 = tableRest.select("stars","review_count");
+
+    //  7. Top 10 restaurants with the most checkin’s in a day
+    val checkin = spark.read.json("data/yelp_academic_dataset_checkin.json")
+    checkin.show()
   }
 }
