482ac192c8e958ae8a92cbc60556aabfc011c9de
revaleo <leo.chen@revature.net>
2022-02-16 11:31:22 -0800

    leo branch Q1, Q2 sort of done

--- src/main/scala/leo_object.scala
+++ src/main/scala/leo_object.scala
@@ -1,9 +1,8 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{from_json,col}
+import org.apache.spark.sql.functions.{col, from_json}
 import org.apache.spark.sql.types.{MapType, StringType}
 
-//codes below are just for the purpose of learning
 
 object project2 extends App {
 
@@ -18,9 +17,10 @@ object project2 extends App {
   val df=spark.read.json("data/yelp_academic_dataset_business.json")
   df.show()
   println(df.count)
-  val df2=spark.read.json("data/yelp_academic_dataset_checkin.json")
-  df2.show()
-  println(df2.count)
+  //val df2=spark.read.json("data/yelp_academic_dataset_checkin.json")
+  //df2.show()
+  //df2.printSchema()
+ // println(df2.count)
   df.printSchema()
   val df3=df.select(col("attributes.*"))
   df3.show()
@@ -29,5 +29,15 @@ object project2 extends App {
   df4.show()
   println(df4.count)
   //df.withColumn("attributes",from_json(col("attributes"),MapType(StringType,StringType))).show()
+
+  //Q1. grouping by city, Sorting by number of business and total review, to find the most popular city
+  val buscount=df.groupBy(col("city")).agg(count("city").as("business_count")).sort(col("business_count").desc)
+  buscount.show()
+  val review=df.groupBy(col("city")).agg(sum("review_count").as("reviewsum")).sort(col("reviewsum").desc)
+  review.show()
+  buscount.join(review,buscount("city") === review("city") ,"right").show()
+  //Q2.
+  df.select(col("stars"), explode(split(col("categories"), ","))).groupBy("col").avg("stars").sort(col("avg(stars)").desc).show()
+  //Q3.
 
 }
