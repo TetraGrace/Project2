7769a394d64b850fcf01e57bdd27ebeef5828c7f
cameronlimRev <cameron.lim@revature.net>
2022-02-16 13:00:02 -0800

    Fixed Branch(cameronlim) Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,11 @@
+import org.apache.spark.sql.SparkSession
+
 import org.apache.spark.sql._
 import scala.io.StdIn._
 
 object Main {
   def main(args: Array[String]):Unit = {
+
     System.setProperty("hadoop.home.dir", "C:\\winutils")
 
     val spark = SparkSession
@@ -12,6 +15,12 @@ object Main {
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val table = spark.read.json()
+    println("Hello World")
+    //i have made a change to the file, and will push to the grace branch
+    //i am not making a change to master
+    println("howdy")
+    print("Try two")
+    println("twy three")
+
   }
 }
