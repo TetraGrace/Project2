43335eb902b5a63dc8c4b0cc7f1668b60f39c665
Hyung Ro (Ryan) Yoon <ryanyoon.gamedev@gmail.com>
2022-02-21 16:53:25 -0500

    Q5 finished. Removed unnecessary codes.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,7 @@ ThisBuild / scalaVersion := "2.12.13"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.1.2",
-  "org.apache.spark" %% "spark-sql" % "3.1.2",
-  "com.github.mrpowers" %% "spark-daria" % "0.39.0"
+  "org.apache.spark" %% "spark-sql" % "3.1.2"
   //"org.apache.spark" %% "spark-hive" % "3.1.2"
 )
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,3 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql._
-import scala.io.StdIn._
 
 object Main {
 
@@ -9,22 +5,11 @@ object Main {
 
   def main(args: Array[String]):Unit = {
 
-    System.setProperty("hadoop.home.dir", "C:\\winutils")
+    Init()
 
-    val spark = SparkSession
-      .builder()
-      .appName("Project2")
-      .config("spark.master", "local")
-      .getOrCreate()
-
-
-    val df = spark.read.json("data/yelp_academic_dataset_business.json")
-    //df.select("business_id").where("attributes = 'RestaurantsTakeout'").show(1000)
-
-    df.select(col("business_id"), col("name"),col("attributes.RestaurantsTableService")).where("attributes.RestaurantsTableService = true").show(100)
   }
 
   def Init():Unit = {
-    //sparkManager.readBusinessData()
+    sparkManager.readBusinessData()
   }
 }
