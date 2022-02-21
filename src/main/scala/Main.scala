02d33864bca1ad01b5fae7d17c5fe3f9888fdaa0
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-02-21 14:58:29 -0500

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
+import scala.io.StdIn._
 
 object Main {
   def main(args: Array[String]):Unit = {
@@ -30,12 +31,25 @@ object Main {
     val q4 = tableRest.select("business_id", "name", "stars", "review_count");
     q4.show()
 
-    //  7. Top 10 restaurants with the most checkin’s in a day
-    val checkin = spark.read.json("data/yelp_academic_dataset_checkin.json")
-    //checkin.show();
-    val temp = tableRest.join(checkin, tableRest("business_id") === checkin("business_id"), "inner");
-    val q7 = temp.select(split(col("date"), ",")).as("date_split");
-    q7.show();
+    //  7. Restaurant comparison metric
+    println("Enter a restaurant's name:")
+    val name = readLine();
+    tableRest.createOrReplaceTempView("tableRest");
+
+    spark.sql(s"create temp view t1 as select city, stars, review_count from tableRest where name = '$name'")
+
+    //match across all locations
+    println("Better than restaurants in all locations")
+    spark.sql("select count(tableRest.name) from tableRest, t1 where tableRest.stars < t1.stars and tableRest.review_count < t1.review_count").show()
+
+    //match to current city only
+    println("Better than restaurants in same city")
+    spark.sql("select count(tableRest.name) from tableRest, t1 where t1.city = tableRest.city and tableRest.stars < t1.stars and tableRest.review_count < t1.review_count").show()
+
+
+    //spark.sql("select q7.business_id, SPLIT(q7.date, ',') as date_array from q7").show();
+    //val q7 = temp.select(split(col("date"), ",")).as("date_split");
+    //q7.show();
 
   }
 }
