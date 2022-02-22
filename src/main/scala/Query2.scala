b649f07ece5fe672d4ea2e74d834a7d0550e1676
cameronlimRev <cameron.lim@revature.net>
2022-02-22 12:11:23 -0800

    Wrapped queries in functions

--- src/main/scala/Query2.scala
+++ src/main/scala/Query2.scala
@@ -15,37 +15,40 @@ object Query2 extends App {
     .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
 
-  //LOAD OUR DATA IN FROM YELP
-  val df=spark.read.json("data/yelp_academic_dataset_business.json")
+  def query2(): Unit= {
 
-  //Create our list of restaurants (Yelp Data includes many more services than restaurants)
-  val templist = Seq("True", "False")
-  val restaurantList = df.filter(df("attributes")("RestaurantsTableService").isin(templist:_*))
-  val restaurantData = restaurantList.select("business_id", "city", "categories", "stars", "review_count")
+    //LOAD OUR DATA IN FROM YELP
+    val df = spark.read.json("data/yelp_academic_dataset_business.json")
 
-  //Create our list of possible cuisine types (filtered manually to remove things like 'Laser Tag')
-  val categoriesDF = spark.read.format("csv").option("header","true").load("data/categories.txt")
-  val categories = categoriesDF.select("Categories").collect()
+    //Create our list of restaurants (Yelp Data includes many more services than restaurants)
+    val templist = Seq("True", "False")
+    val restaurantList = df.filter(df("attributes")("RestaurantsTableService").isin(templist: _*))
+    val restaurantData = restaurantList.select("business_id", "city", "categories", "stars", "review_count")
+
+    //Create our list of possible cuisine types (filtered manually to remove things like 'Laser Tag')
+    val categoriesDF = spark.read.format("csv").option("header", "true").load("data/categories.txt")
+    val categories = categoriesDF.select("Categories").collect()
 
     //Create a table to include all of our categories
-  spark.sql("DROP TABLE IF EXISTS categories")
-  spark.sql("CREATE TABLE IF NOT EXISTS categories (Cuisine String)")
-  for (i <- 0 to categories.length-1){
-    spark.sql(s"INSERT INTO categories VALUES ('${categories(i).toString().replaceAll("[\\[\\]]","")}')")
-  }
-  spark.sql("SELECT * FROM categories").show(1000)
+    spark.sql("DROP TABLE IF EXISTS categories")
+    spark.sql("CREATE TABLE IF NOT EXISTS categories (Cuisine String)")
+    for (i <- 0 to categories.length - 1) {
+      spark.sql(s"INSERT INTO categories VALUES ('${categories(i).toString().replaceAll("[\\[\\]]", "")}')")
+    }
+    spark.sql("SELECT * FROM categories").show(1000)
 
 
-  // Create our ratings table which will include our category and its average rating (stars)
-  spark.sql("DROP TABLE IF EXISTS ratings")
-  spark.sql("CREATE TABLE IF NOT EXISTS ratings (Cuisine String, rating String, count Int)")
-    for (i <- 0 to categories.length-1){
-      var testRating = restaurantData.filter(col("categories").like(s"%${categories(i).toString().replaceAll("[\\[\\]]","")}%"))
+    // Create our ratings table which will include our category and its average rating (stars)
+    spark.sql("DROP TABLE IF EXISTS ratings")
+    spark.sql("CREATE TABLE IF NOT EXISTS ratings (Cuisine String, rating String, count Int)")
+    for (i <- 0 to categories.length - 1) {
+      var testRating = restaurantData.filter(col("categories").like(s"%${categories(i).toString().replaceAll("[\\[\\]]", "")}%"))
       var averageRating = testRating.select(round(avg("stars"), 2)).as("rating")
       var numberReviews = testRating.select(count("review_count").as("Total_Review_count"))
       val ratings = averageRating.collect()
       val reviews = numberReviews.collect()
-      spark.sql(s"INSERT INTO ratings VALUES ('${categories(i).toString().replaceAll("[\\[\\]]","")}',${ratings(0).toString().replaceAll("[\\[\\]]","")},${reviews(0).toString().replaceAll("[\\[\\]]","")} )")
+      spark.sql(s"INSERT INTO ratings VALUES ('${categories(i).toString().replaceAll("[\\[\\]]", "")}',${ratings(0).toString().replaceAll("[\\[\\]]", "")},${reviews(0).toString().replaceAll("[\\[\\]]", "")} )")
     }
-  spark.sql("SELECT * from ratings").orderBy(desc("rating"), desc("count")).show(1000)
+    spark.sql("SELECT * from ratings").orderBy(desc("rating"), desc("count")).show(1000)
+  }
 }
