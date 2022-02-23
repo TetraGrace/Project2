618328f28b89530a3812950beb768c622bb72f00
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-02-23 04:30:46 -0500

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,23 +4,9 @@ import org.apache.spark.sql.SparkSession
 import scala.io.StdIn._
 
 object Main {
-  def main(args: Array[String]):Unit = {
-    
-    System.setProperty("hadoop.home.dir", "C:\\winutils")
-    
-    val spark = SparkSession
-      .builder()
-      .appName("project1")
-      .config("spark.master","local")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    
-    q4();
-    q7();
-    }
-  
-  
-  def q4():Unit = {
+
+  //query 4
+  def q4(spark:SparkSession):Unit = {
     val table = spark.read.json("data/yelp_academic_dataset_business.json")
 
     val df=table.select(col("attributes.*")) //all attributes
@@ -38,9 +24,10 @@ object Main {
     val q4 = tableRest.select("business_id", "name", "stars", "review_count");
     q4.show();
   }
-  
-  
-  def q7():Unit = {
+
+
+  //query 7
+  def q7(spark:SparkSession):Unit = {
     val table = spark.read.json("data/yelp_academic_dataset_business.json")
 
     val df=table.select(col("attributes.*")) //all attributes
@@ -49,7 +36,7 @@ object Main {
     // Getting table of restaurants only
     val templist = Seq("True", "False")
     val tableRest = table.filter(table("attributes")("RestaurantsTableService").isin(templist:_*));
-    
+
     //  7. Restaurant comparison metric
     println("Enter a restaurant's name:")
     val name = readLine();
@@ -65,6 +52,19 @@ object Main {
     println("Better than restaurants in same city")
     spark.sql("select t1.city, count(tableRest.name) from tableRest, t1 where t1.city = tableRest.city and tableRest.stars <= t1.stars and tableRest.review_count < t1.review_count group by t1.city").show()
   }
-  
-  
+
+
+  def main(args: Array[String]):Unit = {
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+    
+    val spark = SparkSession
+      .builder()
+      .appName("project1")
+      .config("spark.master","local")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    
+    q4(spark);
+    q7(spark);
+    }
 }
