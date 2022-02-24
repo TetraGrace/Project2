2ea8d3f6c340ceda9f0e8d2663edf38c33f3edb5
revaleo <97715636+revaleo@users.noreply.github.com>
2022-02-24 12:44:51 -0800

    Update leo_object.scala
    Q1,Q3

--- src/main/scala/leo_object.scala
+++ src/main/scala/leo_object.scala
@@ -33,17 +33,20 @@ object project2 extends App {
   //df.withColumn("attributes",from_json(col("attributes"),MapType(StringType,StringType))).show()
 
   //Q1. grouping by city, Sorting by number of business and total review, to find the most popular city
-  val buscount=df.groupBy(col("city")).agg(count("city").as("business_count")).sort(col("business_count").desc)
+  def Q1:Unit={
+    val buscount=df.groupBy(col("city")).agg(count("city").as("business_count")).sort(col("business_count").desc)
   buscount.show()
   val review=df.groupBy(col("city")).agg(sum("review_count").as("reviewsum")).sort(col("reviewsum").desc)
   review.show()
   buscount.join(review,buscount("city") === review("city") ,"right").show()
+  }
   //Q2.
   df.select(col("stars"), explode(split(col("categories"), ","))).groupBy("col").avg("stars").sort(col("avg(stars)").desc).show()
   //Q3.
-  def business_search:Unit= {
-    val df = spark.read.json("data/yelp_academic_dataset_business.json")
-    val df2= spark.read.json("data/yelp_academic_dataset_checkin.json")
+ def Q3:Unit= {
+    val df = spark.read.json("data/yelp_academic_dataset_business.json").persist(StorageLevel.MEMORY_ONLY_SER)
+    val df2= spark.read.json("data/yelp_academic_dataset_checkin.json").persist(StorageLevel.MEMORY_ONLY_SER)
+
     df.show()
     df2.show()
 
@@ -52,9 +55,15 @@ object project2 extends App {
     val business_df=df.select(col("name"),col("review_count"),col("stars"),col("business_id")).where(col("name")=== business)
     business_df.show()
     val business_id = df2.select(col("business_id")).intersect(business_df.select("business_id")).first.getString(0)
-    val checkin_df=df2.select(explode(split(col("date"),","))).where(col("business_id")===business_id)
+    val checkin_df=df2.select(explode(split(col("date"),", "))).where(col("business_id")===business_id).persist(StorageLevel.MEMORY_ONLY_SER)
     checkin_df.show()
-    checkin_df.write.csv("output")
+    //for zeppelin graphing
+    val zeppelin=checkin_df.select(to_timestamp(col("col"))).withColumn("Month",trunc(col("to_timestamp(col)"), "month"))
+    zeppelin.groupBy("month").count().show
+    //split date and time, not necessary:
+    //checkin_df.withColumn("date",split(col("col")," ").getItem(0)).withColumn("time",split(col("col")," ").getItem(1)).drop("col").show()
+
+    //checkin_df.write.csv("output")
     println(business + "total Check in times is:" + checkin_df.count())
   }
 }
