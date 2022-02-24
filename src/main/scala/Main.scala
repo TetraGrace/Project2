9b785fffd4b04221c855429b63c7a029d9db9dc5
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-02-24 12:06:13 -0500

    fixed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DecimalType
+
 import scala.io.StdIn._
 
 object Main {
@@ -44,17 +46,22 @@ object Main {
     println("Enter a restaurant's name:")
     val name = readLine();
 
-
     spark.sql(s"create temp view t1 as select name, city, stars, review_count from tableRest where name = '$name'")
 
     //match across all locations
     println(s"Out of all $total restaurants, $name is better than:")
-    val q7p1 = spark.sql("select t1.name, t1.city, count(tableRest.name) as ranking from tableRest, t1 where t1.stars >= tableRest.stars and t1.review_count > tableRest.review_count group by t1.name, t1.city")
-    q7p1.show();
+    val q7 = spark.sql("select t1.name, t1.city, count(tableRest.name) as better from tableRest, t1 where t1.stars >= tableRest.stars and t1.review_count > tableRest.review_count group by t1.name, t1.city")
+    val q7t1 = q7.withColumn("total", lit(total));
+    val all_locations = q7t1.withColumn("percentile", col("better") / col("total") * 100);
+    all_locations.show();
 
     //match to city only
     println(s"Out of all restaurants in respective cities, $name is better than:")
-    spark.sql("select t1.name t1.city, count(tableRest.name) as ranking from tableRest, t1 where t1.city = tableRest.city and tableRest.stars <= t1.stars and tableRest.review_count < t1.review_count group by t1.name, t1.city").show()
+    val q7t3 = spark.sql("select t1.name, t1.city, count(tableRest.name) as ranking from tableRest, t1 where t1.city = tableRest.city and tableRest.stars <= t1.stars and tableRest.review_count < t1.review_count group by t1.name, t1.city")
+    val t2 = spark.sql("select tableRest.city as city, count(tableRest.city) as city_count from tableRest join t1 on tableRest.city = t1.city group by tableRest.city")
+    val q7t4 = q7t3.join(t2, "city")
+    val city_locations = q7t4.withColumn("percentile", col("ranking") / col("city_count") * 100)
+    city_locations.show();
   }
 
 
