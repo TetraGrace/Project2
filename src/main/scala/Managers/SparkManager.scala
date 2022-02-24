6dfed69fef6ab0dc28a8478f92ea4eac8976a08f
Tetra Grace Alberts <97699682+TetraGrace@users.noreply.github.com>
2022-02-24 14:30:47 -0600

    Revert "Cameronlim"

---
+++ src/main/scala/Managers/SparkManager.scala
@@ -0,0 +1,20 @@
+package Managers
+
+import org.apache.spark.sql._
+
+class SparkManager {
+
+  val queryManager = new QueryManager
+
+  System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+  val spark = SparkSession.builder()
+    .appName("project2")
+    .config("spark.master", "local")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  def readBusinessData(): Unit = {
+    queryManager.readBusinessData(spark)
+  }
+}
