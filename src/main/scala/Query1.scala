e12bf8ea94aa98af4639cc25ed925e3e1f95da6e
revaleo <97715636+revaleo@users.noreply.github.com>
2022-02-24 13:38:36 -0800

    Update Query1.scala

--- src/main/scala/Query1.scala
+++ src/main/scala/Query1.scala
@@ -4,15 +4,15 @@ import org.apache.spark.sql.functions.{col, count, sum}
 
 object Q1 extends App {
 
-  System.setProperty("hadoop.home.dir", "c:/winutils")
-  val spark = SparkSession
-    .builder()
-    .appName("project1")
-    .config("spark.master", "local")
-    .enableHiveSupport()
-    .getOrCreate()
+//   System.setProperty("hadoop.home.dir", "c:/winutils")
+//   val spark = SparkSession
+//     .builder()
+//     .appName("project1")
+//     .config("spark.master", "local")
+//     .enableHiveSupport()
+//     .getOrCreate()
 
-  val df = spark.read.json("data/yelp_academic_dataset_business.json")
+//  val df = spark.read.json("data/yelp_academic_dataset_business.json")
 
   def Q1: Unit = {
     val buscount = df.groupBy(col("city")).agg(count("city").as("business_count")).sort(col("business_count").desc)
